6c7b2393a8e1fce21f47739b623f864ca5d3bfc1
trmttm <t.yamaka.1213@gmail.com>
2022-01-02 11:37:28 +0400

    Universal button text between Mac and Windows

--- view_tkinter/ViewModels/pickle_loader.py
+++ view_tkinter/ViewModels/pickle_loader.py
@@ -14,7 +14,7 @@ def pickle_loader(frame_id, tree_id='tree_id', pickle_canvas='canvas', button_wi
         f(frame_id, pw_id, 'paned_window', 0, 0, 0, 0, 'nsew', **paned_window_options),
         f(fr1, tree_id, 'treeview', 0, 0, 0, 0, 'nsew', ),
         f(fr1, fr_btn, 'frame', 1, 1, 0, 0, 'nsew', **intf.frame_options(rows_weights, cols_weights)),
-        f(fr_btn, 'button_delete_template', 'button', 1, 1, 0, 0, 'nsew', **{'text': '❌', 'width': button_width}),
+        f(fr_btn, 'button_delete_template', 'button', 1, 1, 0, 0, 'nsew', **{'text': 'Del', 'width': button_width}),
         f(fr_btn, 'button_load_pickle', 'button', 1, 1, 1, 1, 'nsew', **{'text': 'Load', 'width': button_width}),
         f(fr_btn, 'button_merge_pickle', 'button', 1, 1, 2, 2, 'nsew', **{'text': 'Merge', 'width': button_width}),
         f(fr2, pickle_canvas, 'canvas', 0, 0, 0, 0, 'nsew', bg='light yellow'),
