9fe610d7cd20ee5e078eb9b28c4c93db7e23c41c
trmttm <t.yamaka.1213@gmail.com>
2022-01-02 11:32:55 +0400

    Universal button text between Mac and Windows

--- view_tkinter/ViewModels/dpad.py
+++ view_tkinter/ViewModels/dpad.py
@@ -4,9 +4,9 @@ from .. import tk_interface as intf
 def create_d_pad_model(frame_id, button_ids_x4: tuple, button_width=None) -> list:
     f = intf.widget_model
     b = button_ids_x4
-    view_model = [f(frame_id, b[0], 'button', 0, 0, 0, 0, 'nsew', **{'text': '⬅️', 'width': button_width}),
-                  f(frame_id, b[1], 'button', 0, 0, 1, 1, 'nsew', **{'text': '⬆️', 'width': button_width}),
-                  f(frame_id, b[2], 'button', 0, 0, 2, 2, 'nsew', **{'text': '⬇️', 'width': button_width}),
-                  f(frame_id, b[3], 'button', 0, 0, 3, 3, 'nsew', **{'text': '➡️', 'width': button_width}),
+    view_model = [f(frame_id, b[0], 'button', 0, 0, 0, 0, 'nsew', **{'text': '←', 'width': button_width}),
+                  f(frame_id, b[1], 'button', 0, 0, 1, 1, 'nsew', **{'text': '↑', 'width': button_width}),
+                  f(frame_id, b[2], 'button', 0, 0, 2, 2, 'nsew', **{'text': '↓', 'width': button_width}),
+                  f(frame_id, b[3], 'button', 0, 0, 3, 3, 'nsew', **{'text': '→', 'width': button_width}),
                   ]
     return view_model
--- view_tkinter/ViewModels/operator_buttons.py
+++ view_tkinter/ViewModels/operator_buttons.py
@@ -4,8 +4,8 @@ from .. import tk_interface as intf
 def operator_buttons(frame_id, button_width=None) -> list:
     f = intf.widget_model
     view_model = [
-        f(frame_id, 'op1', 'button', 0, 0, 0, 0, 'nsew', **{'text': '➕', 'width': button_width}),
-        f(frame_id, 'op2', 'button', 0, 0, 1, 1, 'nsew', **{'text': '➖', 'width': button_width}),
-        f(frame_id, 'op3', 'button', 0, 0, 2, 2, 'nsew', **{'text': '✖️', 'width': button_width}),
-        f(frame_id, 'op4', 'button', 0, 0, 3, 3, 'nsew', **{'text': '➗', 'width': button_width}), ]
+        f(frame_id, 'op1', 'button', 0, 0, 0, 0, 'nsew', **{'text': '+', 'width': button_width}),
+        f(frame_id, 'op2', 'button', 0, 0, 1, 1, 'nsew', **{'text': '-', 'width': button_width}),
+        f(frame_id, 'op3', 'button', 0, 0, 2, 2, 'nsew', **{'text': 'x', 'width': button_width}),
+        f(frame_id, 'op4', 'button', 0, 0, 3, 3, 'nsew', **{'text': '/', 'width': button_width}), ]
     return view_model
