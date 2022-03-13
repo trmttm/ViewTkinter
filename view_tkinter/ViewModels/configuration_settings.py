768768440b10bb14e94b8fcf02044288c2098f68
trmttm <96355189+trmttm@users.noreply.github.com>
2022-03-13 10:46:40 +0400

    Now user can choose where to place auto relay.     1) Right end of the sheet, or     2) Next to connection_to.

--- view_tkinter/ViewModels/configuration_settings.py
+++ view_tkinter/ViewModels/configuration_settings.py
@@ -11,6 +11,7 @@ def create_configuration_setting(frame, **kwargs):
     btn_setting_ok = kwargs['btn_setting_ok']
     check_btn_cleaner = kwargs['check_btn_cleaner'] if 'check_btn_cleaner' in kwargs else 'check_btn_cleaner'
     check_btn_live_calc = kwargs['check_btn_live_calc'] if 'check_btn_live_calc' in kwargs else 'check_btn_live_calc'
+    check_btn_auto_relay_to_right_end = kwargs.get('check_btn_relay_right_end', 'check_btn_relay_right_end')
     btn_project = kwargs.get('btn_project', 'btn_project')
 
     entry_account_w = kwargs.get('account_w')
@@ -25,6 +26,7 @@ def create_configuration_setting(frame, **kwargs):
     entry_bb_w = kwargs.get('bb_w')
     entry_bb_h = kwargs.get('bb_h')
     entry_bb_font_size = kwargs.get('bb_font_size')
+    frame_account = 'account_frame'
 
     view_model = [
         wm(frame, fr, 'frame', 0, 0, 0, 0, 'nsew', **intf.frame_options(*rc_0_1)),
@@ -42,26 +44,29 @@ def create_configuration_setting(frame, **kwargs):
         wm(fr, 'live_calculation', 'label', 3, 3, 0, 0, 'nsew', **{'text': 'Live Calculation'}),
         wm(fr, check_btn_live_calc, 'check_button', 3, 3, 1, 1, 'w', **{'value': True}),
 
-        wm(fr, 'account_frame', 'frame', 4, 4, 0, 2, 'nsew', ),
-        wm('account_frame', 'account', 'label', 0, 0, 0, 0, 'nsew', **{'text': 'Account'}),
-        wm('account_frame', entry_account_w, 'entry', 0, 0, 1, 1, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_account_h, 'entry', 0, 0, 2, 2, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_account_font_size, 'entry', 0, 0, 3, 3, 'nsew', **{'width': 3}),
+        wm(fr, frame_account, 'frame', 4, 4, 0, 2, 'nsew', ),
+        wm(frame_account, 'account', 'label', 0, 0, 0, 0, 'nsew', **{'text': 'Account'}),
+        wm(frame_account, entry_account_w, 'entry', 0, 0, 1, 1, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_account_h, 'entry', 0, 0, 2, 2, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_account_font_size, 'entry', 0, 0, 3, 3, 'nsew', **{'width': 3}),
 
-        wm('account_frame', 'operator', 'label', 1, 1, 0, 0, 'nsew', **{'text': 'Operator'}),
-        wm('account_frame', entry_operator_w, 'entry', 1, 1, 1, 1, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_operator_h, 'entry', 1, 1, 2, 2, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_operator_font_size, 'entry', 1, 1, 3, 3, 'nsew', **{'width': 3}),
+        wm(frame_account, 'operator', 'label', 1, 1, 0, 0, 'nsew', **{'text': 'Operator'}),
+        wm(frame_account, entry_operator_w, 'entry', 1, 1, 1, 1, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_operator_h, 'entry', 1, 1, 2, 2, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_operator_font_size, 'entry', 1, 1, 3, 3, 'nsew', **{'width': 3}),
 
-        wm('account_frame', 'constant', 'label', 2, 2, 0, 0, 'nsew', **{'text': 'Constant'}),
-        wm('account_frame', entry_constant_w, 'entry', 2, 2, 1, 1, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_constant_h, 'entry', 2, 2, 2, 2, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_constant_font_size, 'entry', 2, 2, 3, 3, 'nsew', **{'width': 3}),
+        wm(frame_account, 'constant', 'label', 2, 2, 0, 0, 'nsew', **{'text': 'Constant'}),
+        wm(frame_account, entry_constant_w, 'entry', 2, 2, 1, 1, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_constant_h, 'entry', 2, 2, 2, 2, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_constant_font_size, 'entry', 2, 2, 3, 3, 'nsew', **{'width': 3}),
 
-        wm('account_frame', 'bb', 'label', 3, 3, 0, 0, 'nsew', **{'text': 'BB'}),
-        wm('account_frame', entry_bb_w, 'entry', 3, 3, 1, 1, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_bb_h, 'entry', 3, 3, 2, 2, 'nsew', **{'width': 3}),
-        wm('account_frame', entry_bb_font_size, 'entry', 3, 3, 3, 3, 'nsew', **{'width': 3}),
+        wm(frame_account, 'bb', 'label', 3, 3, 0, 0, 'nsew', **{'text': 'BB'}),
+        wm(frame_account, entry_bb_w, 'entry', 3, 3, 1, 1, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_bb_h, 'entry', 3, 3, 2, 2, 'nsew', **{'width': 3}),
+        wm(frame_account, entry_bb_font_size, 'entry', 3, 3, 3, 3, 'nsew', **{'width': 3}),
+
+        wm(fr, 'lbl_relay_x', 'label', 5, 5, 0, 0, 'nsew', **{'text': 'Auto relay at right end'}),
+        wm(fr, check_btn_auto_relay_to_right_end, 'check_button', 5, 5, 1, 1, 'w', **{'value': True}),
 
     ]
     return view_model
